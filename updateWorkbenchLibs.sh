74dc4ab74ab52a3b56d1ac8466ae383474cec4a3
hammyau <blueham60@gmail.com>
2024-05-09 10:11:44 +0800

    RCA added to update script
    Signed-off-by: hammyau <blueham60@gmail.com>

--- updateWorkbenchLibs.sh
+++ updateWorkbenchLibs.sh
@@ -1,4 +1,5 @@
 cp RunControlGenerator/target/runcontrolgenerator-1.0.2.jar ../Workbench/plugins/genevagui/lib/
+cp RunControlAnalyser/target/runcontrolanalyser-1.0.2.jar ../Workbench/plugins/genevagui/lib/
 cp Compilers/target/compilers-1.0.2.jar ../Workbench/plugins/genevagui/lib/
 cp GenevaIO/target/genevaio-1.0.2.jar ../Workbench/plugins/genevagui/lib/
 cp Repository/target/repository-1.0.2.jar ../Workbench/plugins/genevagui/lib/
