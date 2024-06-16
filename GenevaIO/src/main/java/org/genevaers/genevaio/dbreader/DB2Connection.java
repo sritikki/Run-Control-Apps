0cf5a56533f16b5124335205b90147b22d285611
hammyau <blueham60@gmail.com>
2024-06-17 07:47:53 +0800

    DB2 connect user only test
    Signed-off-by: hammyau <blueham60@gmail.com>

--- GenevaIO/src/main/java/org/genevaers/genevaio/dbreader/DB2Connection.java
+++ GenevaIO/src/main/java/org/genevaers/genevaio/dbreader/DB2Connection.java
@@ -24,7 +24,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Properties;
+import com.ibm.db2.jcc.*;      
 import com.google.common.flogger.FluentLogger;
 
 import java.sql.ResultSet;
@@ -48,6 +49,19 @@ public class DB2Connection extends DatabaseConnection{
 
         con = DriverManager.getConnection(url, params.getUsername(), params.getPassword());
    }
+
+   public void connectUserOnly() throws SQLException {
+
+        Properties properties = new Properties(); 
+        properties.put("user", params.getUsername()); 
+        properties.put("passowrd", params.getPassword()); 
+        properties.put("port", params.getPort()); 
+        properties.put("securityMechanism", new String("" + com.ibm.db2.jcc.DB2BaseDataSource.USER_ONLY_SECURITY + "")); 
+        String url = "jdbc:db2://" + params.getServer()+ ":"
+        + params.getPort() + "/"
+        + params.getDatabase();
+        con = DriverManager.getConnection(url, properties); 
+    }
 
     @Override   
     public boolean isConnected() {
