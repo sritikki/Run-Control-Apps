a607db66bf70c0e74b8eecdb39dc8188a6d69ad3
hammyau <blueham60@gmail.com>
2024-10-23 17:42:07 +0800

    do not default CFEL date code to NONE
    Signed-off-by: hammyau <blueham60@gmail.com>

--- Compilers/src/main/java/org/genevaers/compilers/extract/emitters/comparisonemitters/CFELEmitter.java
+++ Compilers/src/main/java/org/genevaers/compilers/extract/emitters/comparisonemitters/CFELEmitter.java
@@ -49,7 +49,6 @@ public class CFELEmitter extends ComparisonEmitter{
         arg.setLrId(lkf.getRef().getLrID());
         arg.setFieldId(lkf.getRef().getComponentId());
         arg.setStartPosition(redFld.getStartPosition());
-        arg.setFieldContentId(DateCode.NONE); //TODO unsure is this is always the case?
         cfel.setArg2(arg);
         return cfel;
     }
